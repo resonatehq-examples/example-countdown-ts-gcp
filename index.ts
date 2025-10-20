ad99ec1f42d57f948efa0b5c26c2a98829ee7d91
Tomperez98 <tomasperezalvarez@gmail.com>
2025-10-20 11:54:05 -0500

    Factorial

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Resonate } from "@resonatehq/gcp";
+import type { Context } from "@resonatehq/sdk";
+
+const resonate = new Resonate();
+
+function* factorial(ctx: Context, n: number): Generator<any, number, any> {
+	if (n <= 1) {
+		return 1;
+	}
+	return n * (yield ctx.rpc("factorial", n - 1));
+}
+
+resonate.register("factorial", factorial);
+
+export const handler = resonate.handlerHttp();
